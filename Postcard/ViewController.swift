7557a0721f17f311fd10de98233408ab2a1c3762
Riz Nwosu <Riz@Riz-Nwosus-MacBook-Pro.local>
2014-10-21 23:24:49 -0700

    added a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Just made some changes.
     }
 
     override func didReceiveMemoryWarning() {
